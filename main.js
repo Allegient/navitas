23b3474672880412846c8ed8497e8a7bd811e783
Timon Leung <timon.leung@tpgtelecom.com.au>
2023-03-14 14:04:59 +0800

    functios added

--- main.js
+++ main.js
@@ -2,4 +2,82 @@ export default class Main {
     constructor() {
     }
 
+    validateStudentID(surName, firstName, studentID) {
+        let surname = this.getSurname(surName);;
+        let firstname = this.getFirstName(firstName);;
+        let studentid = this.getJoinDateAndHash(studentID);
+
+
+
+    }
+
+    getHash(studentID) {
+        let sumOfOdds = this.getSumOfOdds(studentID);
+        let sumOfEvens = this.getSumOfEvens(studentID);
+        let difference = Math.abs(sumOfEvens - sumOfOdds);
+
+
+        if (difference > 9) {
+            return this.calculateHash(difference);
+        }
+
+        return sumOfEvens - sumOfOdds;
+    }
+
+    calculateHash(number) {
+        return number % 10;
+    }
+
+    getSumOfOdds(studentID) {
+        let fullDate = this.getJoinDateAndHash(studentID);
+
+        let dateArray = Array.from(String(fullDate), Number);
+        let odds = dateArray.filter(function(v, i) {return i % 2 == 0;
+        });
+
+        const sum = odds.reduce((partialSum, a) => partialSum + a, 0);
+
+
+        return sum;
+    }
+
+    getSumOfEvens(studentID) {
+        let fullDate = this.getJoinDateAndHash(studentID);
+
+        let dateArray = Array.from(String(fullDate), Number);
+        let evens = dateArray.filter(function(v, i) {return i % 2 == 1;
+        });
+
+        const sum = evens.reduce((partialSum, a) => partialSum + a, 0);
+
+
+        return sum;
+    }
+
+    getSurname(studentID) {
+        let studentIDArray = studentID.split('');
+        let surname = studentIDArray[0] + studentID[1];
+        return surname;
+    }
+
+    getFirstName(studentID) {
+        let studentIDArray = studentID.split('');
+        let firstName = studentIDArray[2] + studentID[3];
+        return firstName;
+    }
+
+    getJoinDateAndHash(studentID) {
+        let studentIDArray = studentID.split('');
+        let date = [];
+
+        for (let i = 4; studentIDArray.length - 1 > i; i++) {
+            date.push(studentIDArray[i]);
+        }
+        let joinedDate = date.join('');
+
+
+        return joinedDate;
+    }
+
+
 }
--- main.test.js
+++ main.test.js
@@ -8,4 +8,35 @@ describe('Unit Testing Main', () => {
     })
 
 
+    it('Should return the first 2 letters of surname', () => {
+        expect(main.getSurname('SHRO202208017')).toEqual('SH');
+    })
+
+
+    it('Should return the first 2 letters of firstname', () => {
+        expect(main.getFirstName('SHRO202208017')).toEqual('RO');
+    })
+
+    it('Should return date', () => {
+        expect(main.getJoinDateAndHash('SHRO202208017')).toEqual('20220801');
+    })
+
+
+    it('Sum of Odds', () => {
+        expect(main.getSumOfOdds('SHRO202208017')).toEqual(4)
+    })
+
+    it('Sum of Evens', () => {
+        expect(main.getSumOfEvens('SHRO202208017')).toEqual(11)
+    })
+
+    it('Returns hash of student ID', () => {
+        expect(main.getHash('SHRO809200017')).toEqual(7)
+    })
+
+    it('Validate student ID', () => {
+        expect(main.validateStudentID('SHRO809200017')).toEqual(true);
+
+    })
+
 })
